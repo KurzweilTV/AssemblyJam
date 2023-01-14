aa34e5c2db0d32c8a7781c9363cd152a7d79b2f5
Ktosox <ktoso3@gmail.com>
2023-01-14 23:02:28 +0100

    even more shaders
    + more noise textures
    + fixes to sound manager

--- Managers/SoundManager.gd
+++ Managers/SoundManager.gd
@@ -1,14 +1,15 @@
 extends Node
 
 
-
 func play_song(newSong : AudioStream):
 	if $MusicPlayer1.playing:
 		$MusicPlayer2.stream = newSong
 		$Tranistion.play("Play2")
+		$MusicPlayer2.play()
 	else:
 		$MusicPlayer1.stream = newSong
 		$Tranistion.play("Play1")
+		$MusicPlayer1.play()
 	pass
 
 
