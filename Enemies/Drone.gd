a9b6de3a8b88343d585aa15145e277a3c39b28b4
Ktosox <ktoso3@gmail.com>
2023-01-15 15:19:53 +0100

    last changes before test build

--- Enemies/Drone.gd
+++ Enemies/Drone.gd
@@ -1,18 +1,20 @@
 extends Enemy
 
-var speedMax = 5000
+var speedMax = 11000
 
 var grenadeScene = preload("res://Enemies/Grenade.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Floatyness.call_deferred("advance",randf()*2.0)
 	set_process(false)
 	pass # Replace with function body.
 
 
 func _process(delta):
-	applied_force += global_position.direction_to(GlobalPlayer.controller.global_position + Vector2(0,-220) ) * delta * 4000
+	applied_force += global_position.direction_to(GlobalPlayer.controller.global_position + Vector2(0,-220) ) * delta * 20000
 	applied_force.y -= applied_force.y * delta
+	applied_force += Vector2(randf()-0.5,randf()-0.5) * 600
 	applied_force = applied_force.limit_length(speedMax)
 
 
@@ -36,17 +38,13 @@ func _on_DetectionRange_body_entered(body):
 	pass # Replace with function body.
 
 
-func _on_GroundScanner_body_entered(body):
-	while $GroundScanner.overlaps_body(body):
-		apply_central_impulse(Vector2(rand_range(-200,200), -1490 * (randf()+1.2 ) ))
-		yield(get_tree().create_timer(0.3),"timeout")
-	pass # Replace with function body.
 
 
 func _on_AttackTimer_timeout():
 	if $DetectionRange.get_overlapping_bodies().size()>0:
 		var newAttack = grenadeScene.instance()
+		var targetLoc = Vector2(0,-50) + GlobalPlayer.controller.global_position as Vector2 # where the player at
 		newAttack.global_position = global_position
-		newAttack.apply_central_impulse(lerp(Vector2.UP,global_position.direction_to(Vector2(0,-100)+GlobalPlayer.controller.global_position),0.5) * 500)
+		newAttack.apply_central_impulse(lerp(Vector2.UP,global_position.direction_to(targetLoc),0.7) * min(900,targetLoc.distance_squared_to(global_position)) )
 		get_parent().add_child(newAttack)
 	pass # Replace with function body.
