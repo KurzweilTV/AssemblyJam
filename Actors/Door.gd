85552481bd81bd26e20d7c244aac897798eefe5f
Kurzweil <m.barr@mac.com>
2023-01-15 11:27:44 +0100

    adds countdown plugin and project icon

--- Actors/Door.gd
+++ Actors/Door.gd
@@ -8,13 +8,13 @@ func _ready() -> void:
 
 func door_open() -> void:
 	var tween = create_tween()
-	tween.tween_property(top, "position:y", -150.0, 2.0)
-	tween.parallel().tween_property(bottom, "position:y", 150.0, 2.0)
+	tween.tween_property(top, "position:y", -150.0, 0.3)
+	tween.parallel().tween_property(bottom, "position:y", 150.0, 0.3)
 
 func door_close() -> void:
 	var tween = create_tween()
-	tween.tween_property(top, "position:y", -48.0, 2.0)
-	tween.parallel().tween_property(bottom, "position:y", 48.0, 2.0)
+	tween.tween_property(top, "position:y", -48.0, 0.3)
+	tween.parallel().tween_property(bottom, "position:y", 48.0, 0.3)
 
 func _door_sensor_on(area: Area2D) -> void:
 	door_open()
