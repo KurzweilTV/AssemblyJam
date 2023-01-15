3c75dc3c1142b06cb6ca64bbc122647ea8dcd50b
Kurzweil <m.barr@mac.com>
2023-01-15 13:51:06 +0100

    door tweaks

--- Actors/Door.gd
+++ Actors/Door.gd
@@ -8,13 +8,13 @@ func _ready() -> void:
 
 func door_open() -> void:
 	var tween = create_tween()
-	tween.tween_property(top, "position:y", -150.0, 0.3)
-	tween.parallel().tween_property(bottom, "position:y", 150.0, 0.3)
+	tween.tween_property(top, "position:y", (top.position.y - 100), 0.2)
+	tween.parallel().tween_property(bottom, "position:y", (bottom.position.y + 100), 0.2)
 
 func door_close() -> void:
 	var tween = create_tween()
-	tween.tween_property(top, "position:y", -48.0, 0.3)
-	tween.parallel().tween_property(bottom, "position:y", 48.0, 0.3)
+	tween.tween_property(top, "position:y", (top.position.y + 100), 0.3)
+	tween.parallel().tween_property(bottom, "position:y", (bottom.position.y - 100), 0.3)
 
 func _door_sensor_on(area: Area2D) -> void:
 	door_open()
