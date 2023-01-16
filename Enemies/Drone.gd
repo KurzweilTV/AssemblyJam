dda691c3f4cc1a33ec1234f16974ad6718dfa24d
Ktosox <ktoso3@gmail.com>
2023-01-16 22:21:53 +0100

    Added second enemy
    also did some clean up in Drone

--- Enemies/Drone.gd
+++ Enemies/Drone.gd
@@ -4,25 +4,28 @@ var speedMax = 11000
 
 var grenadeScene = preload("res://Enemies/Grenade.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Floatyness.call_deferred("advance",randf()*2.0)
-	set_process(false)
-	pass # Replace with function body.
+	set_physics_process(false)
 
-
-func _process(delta):
+func _physics_process(delta):
 	applied_force += global_position.direction_to(GlobalPlayer.controller.global_position + Vector2(0,-220) ) * delta * 20000
 	applied_force.y -= applied_force.y * delta
 	applied_force += Vector2(randf()-0.5,randf()-0.5) * 600
 	applied_force = applied_force.limit_length(speedMax)
 
-
+func attack():
+	var newAttack = grenadeScene.instance()
+	var targetLoc = Vector2(0,-50) + GlobalPlayer.controller.global_position as Vector2 # where the player at
+	newAttack.global_position = global_position
+	newAttack.apply_central_impulse(lerp(Vector2.UP,global_position.direction_to(targetLoc),0.7) * min(900,targetLoc.distance_squared_to(global_position)) )
+	get_tree().current_scene.add_child(newAttack)
+	pass
 
 func die():
 	dead = true
 	$Animator.play("Death")
-	set_process(false)
+	set_physics_process(false)
 	$AttackTimer.stop()
 	$HitBox/CollisionShape2D.set_deferred("disabled",true) 
 	$CollisionShape2D.set_deferred("disabled",true)
@@ -30,24 +33,12 @@ func die():
 func _on_Animator_animation_finished(anim_name):
 	if anim_name == "Death":
 		queue_free()
-	pass # Replace with function body.
 
-
-func _on_DetectionRange_body_entered(body):
-	# player came close
+func _on_DetectionRange_body_entered(body): # player came close
 	assert(body.is_in_group("Player"))
-	set_process(true)
+	set_physics_process(true)
 	$AttackTimer.start()
-	pass # Replace with function body.
-
-
-
 
 func _on_AttackTimer_timeout():
 	if $DetectionRange.get_overlapping_bodies().size()>0:
-		var newAttack = grenadeScene.instance()
-		var targetLoc = Vector2(0,-50) + GlobalPlayer.controller.global_position as Vector2 # where the player at
-		newAttack.global_position = global_position
-		newAttack.apply_central_impulse(lerp(Vector2.UP,global_position.direction_to(targetLoc),0.7) * min(900,targetLoc.distance_squared_to(global_position)) )
-		get_parent().add_child(newAttack)
-	pass # Replace with function body.
+		attack()
---
+++ Enemies/Tanky.gd
@@ -0,0 +1,60 @@
+extends Enemy
+
+var bulletScene = preload("res://Enemies/Bullet.tscn")
+var moveSpeed = 340
+
+var shooting = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func turn_around():
+	applied_force.x *= -1
+	$Sprite.scale.x *= -1
+	$DetectionRange.scale.x *= -1
+	pass
+
+func resume():
+	shooting = false
+	applied_force.x = moveSpeed * $Sprite.scale.x
+	pass
+
+func fire():
+	applied_force.x = 0
+	$FireTimer.start()
+	var newBullet = bulletScene.instance()
+	newBullet.global_position = $Sprite/Line2D2/Muzzle.global_position
+	newBullet.add_force(Vector2(),Vector2($Sprite.scale.x*2000,0))
+	newBullet.set_meta("damage",1.0)
+	newBullet.scale.x = $Sprite.scale.x
+	get_tree().current_scene.add_child(newBullet)
+	pass
+
+func _on_Tanky_body_entered(body):
+	turn_around()
+	pass # Replace with function body.
+
+
+func _on_GroundTimer_timeout():
+	$Sprite/GroundScanner.update()
+	if !$Sprite/GroundScanner.is_colliding():
+		turn_around()
+		yield(get_tree().create_timer(0.7),"timeout")
+
+
+func _on_DetectionRange_body_entered(body):
+	assert(body.is_in_group("Player"))
+	if !shooting:
+		shooting = true
+		fire()
+	pass # Replace with function body.
+
+
+func _on_FireTimer_timeout():
+	if $DetectionRange.get_overlapping_bodies().size() > 0:
+		fire()
+	else:
+		resume()
+		
+	pass # Replace with function body.
