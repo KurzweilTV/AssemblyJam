2dd866e50535fcd7887206345ed2bfa71882c814
Ktosox <ktoso3@gmail.com>
2023-01-17 16:02:32 +0100

    added fade in/out on level load

--- UI/UI.gd
+++ UI/UI.gd
@@ -7,5 +7,14 @@ func update_health(newHealth):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	visible = true
+	$AnimationPlayer.play("In")
+	get_tree().paused = true
 	pass # Replace with function body.
 
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "In":
+		get_tree().paused = false
+	pass # Replace with function body.
