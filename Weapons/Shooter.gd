dc8bb63cd67de73737496c6c8e97f0fd364796b3
Ktosox <ktoso3@gmail.com>
2023-01-20 17:28:22 +0100

    more gun stuff

--- Weapons/Shooter.gd
+++ Weapons/Shooter.gd
@@ -5,4 +5,8 @@ var damage
 export var projectile : Resource # the bullet that will be shot
 
 func shoot():
+	var newPew = projectile.instance() as RigidBody2D
+	newPew.set_meta("damage",damage)
+	newPew.global_position = global_position
+	newPew.global_rotation = global_rotation
 	pass
