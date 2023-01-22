38c2a91ba295533f64795ef1f3ef39b54c001de5
Ktosox <ktoso3@gmail.com>
2023-01-22 17:58:01 +0100

    added death animation to dorne boss

--- Boss/BossDrone.gd
+++ Boss/BossDrone.gd
@@ -7,6 +7,7 @@ var phase = 1
 export var health = 50
 
 
+
 func get_damaged(dmg):
 	if phase == 1:
 		return
@@ -18,8 +19,12 @@ func get_damaged(dmg):
 	
 	
 func die():
-	emit_signal("boss_death")
-	queue_free()
+	for G in $Guns.get_children():
+		G.shooting = false
+	$PhaseTimer.stop()
+	$AnimationPlayer.stop()
+	$DeathAnimation.play("New Anim")
+	$JunkGenerator.play()
 
 func start_boss():
 	$AnimationPlayer.play("New Anim")
@@ -32,10 +37,18 @@ func _on_PhaseTimer_timeout():
 		for G in $Guns.get_children():
 			G.fire()
 			G.rotation_degrees = 270 + (randf()-0.5) * 50
-			$Shield.visible = true
+		$Shield.visible = true
 		pass
 	else:
 		for G in $Guns.get_children():
 			G.shooting = false
-			$Shield.visible = false
+		$Shield.visible = false
 	pass # Replace with function body.
+
+
+func _on_DeathAnimation_animation_finished(anim_name):
+	emit_signal("boss_death")
+	$JunkGenerator.stop()
+	queue_free()
+	pass # Replace with function body.
+
